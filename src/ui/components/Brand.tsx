87661351d1f11912ab9c4870147d3d39b2e66ee5
Marv <rotrobmin@gmail.com>
2026-03-14 23:13:16 -0700

    fix: revert logo rendering to png assets

--- src/ui/components/Brand.tsx
+++ src/ui/components/Brand.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import duckgradesLogo from "../../assets/duckgrades.svg";
+import duckgradesLogo from "../../assets/duckgrades.png";
 
 type BrandProps = {
   home?: boolean;
